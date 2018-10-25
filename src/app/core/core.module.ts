535c4a77eff62da6c0b38d7c392ce1f2857d184b
Joshua Dizon <joshuagad@gmail.com>
2018-10-25 13:48:07 +0800

    Add scroll ngx-page-scroll

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { NgxPageScrollModule } from 'ngx-page-scroll';
+
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    NgxPageScrollModule
   ],
   declarations: [],
   exports: []
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject, ViewChild, ElementRef } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { PageScrollInstance, PageScrollService, PageScrollConfig } from 'ngx-page-scroll';
 
 @Component({
   selector: 'app-navbar',
@@ -6,8 +8,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
+  @ViewChild('container')
+  private container: ElementRef;
 
-  constructor() { }
+  constructor(
+    private pageScrollService: PageScrollService,
+    @Inject(DOCUMENT) private document: any
+  ) { 
+    PageScrollConfig.defaultDuration = 750;
+    PageScrollConfig.defaultEasingLogic = {
+      ease: (t: number, b: number, c: number, d: number): number => {
+        // easeInOutExpo easing
+        if (t === 0) return b;
+        if (t === d) return b + c;
+        if ((t /= d / 2) < 1) return c / 2 * Math.pow(2, 10 * (t - 1)) + b;
+        return c / 2 * (-Math.pow(2, -10 * --t) + 2) + b;
+      }
+    };
+  }
+
+  public scrollTo(id): void {
+     let pageScrollInstance: PageScrollInstance = PageScrollInstance.simpleInstance(this.document, id);
+     this.pageScrollService.start(pageScrollInstance);
+   };    
 
   ngOnInit() {
   }
